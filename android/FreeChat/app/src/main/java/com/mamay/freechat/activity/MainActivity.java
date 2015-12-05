2d47d30f0fdbebc6cb0a846d2ad42af0896514fa
Dmytro Dashenkov <ymhik98@gmail.com>
2015-12-05 18:56:09 +0200

    moved some files

--- android/FreeChat/app/src/main/java/com/mamay/freechat/activity/MainActivity.java
+++ android/FreeChat/app/src/main/java/com/mamay/freechat/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mamay.freechat;
+package com.mamay.freechat.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.mamay.freechat.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -39,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
--- android/FreeChat/app/src/main/java/com/mamay/freechat/fragment/MainActivityFragment.java
+++ android/FreeChat/app/src/main/java/com/mamay/freechat/fragment/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package com.mamay.freechat;
+package com.mamay.freechat.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.mamay.freechat.R;
 
 /**
  * A placeholder fragment containing a simple view.
