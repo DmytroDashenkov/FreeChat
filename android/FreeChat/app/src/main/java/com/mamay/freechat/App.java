9462bb736f2bc367b36d486e0a4379f98c472ab1
Dmytro Dashenkov <ymhik98@gmail.com>
2015-12-05 20:21:32 +0200

    problems fixed

--- android/FreeChat/app/src/main/java/com/mamay/freechat/App.java
+++ android/FreeChat/app/src/main/java/com/mamay/freechat/App.java
@@ -8,17 +8,13 @@ public class App extends Application{
 
     public static LoginManager loginManager;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        loginManager = new LoginManager();
+    public static LoginManager getLoginManager() {
+        return loginManager;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
         loginManager = new LoginManager();
-    }    public static LoginManager getLoginManager() {
-        return loginManager;
     }
 }
