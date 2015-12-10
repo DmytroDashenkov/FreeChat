1cc88642384e09859eff40922fa374548d6756a5
Dmytro Dashenkov <ymhik98@gmail.com>
2015-12-10 10:55:44 +0200

    getting custom attrs from xml

--- android/FreeChat/app/src/main/java/com/mamay/freechat/view/LoginButtonMergedView.java
+++ android/FreeChat/app/src/main/java/com/mamay/freechat/view/LoginButtonMergedView.java
@@ -2,6 +2,7 @@ package com.mamay.freechat.view;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.mamay.freechat.R;
 
 public class LoginButtonMergedView extends RelativeLayout {
 
@@ -52,5 +55,24 @@ public class LoginButtonMergedView extends RelativeLayout {
 
         ImageView icon = new ImageView(context);
         addView(icon, imageParams);
+    }
+
+    private void assignContent(Context context, AttributeSet attrs) {
+
+        String title = "";
+        int iconLink = 0;
+        int bgColor = 0;
+
+        TypedArray customAttrs = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.LoginButtonMergedView, 0, 0);
+        try {
+            title = customAttrs.getString(R.styleable.LoginButtonMergedView_loginText);
+            iconLink = customAttrs.getInt(R.styleable.LoginButtonMergedView_loginIcon,
+                    R.mipmap.ic_launcher);
+            bgColor = customAttrs.getColor(R.styleable.LoginButtonMergedView_loginBackgroundColor,
+                    0xFFF);
+        } finally {
+            customAttrs.recycle();
+        }
     }
 }
