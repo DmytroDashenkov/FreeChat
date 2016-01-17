4389a3da48c38c44aa4ba62f4cf8805a3066427f
Dmytro Dashenkov <ymhik98@gmail.com>
2016-01-17 17:29:50 +0200

    deleted garbage + sever sample changes

--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,16 @@
 var http = require("http");
+var fs = require("fs");
 
 http.createServer(function (req, res) {
 
     res.writeHead(200);
-    res.write(req.toString());
+    fs.readFile("./public/file.html", function(err, data) {
+        if(err)
+            res.write(err.toString());
+        else
+            res.write(data.toString());
+
+        res.end("HA-HA-HA");
+    });
 
 }).listen(8080);
